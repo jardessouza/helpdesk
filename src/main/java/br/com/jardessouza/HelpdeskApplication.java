f2d38b20dd6cb026a639300918d445c9bf939680
Jardes Souza <jardessouza@gmail.com>
2023-04-08 16:33:09 -0300

    Adicionado h2, MySql e perfis

--- src/main/java/br/com/jardessouza/HelpdeskApplication.java
+++ src/main/java/br/com/jardessouza/HelpdeskApplication.java
@@ -1,13 +1,24 @@
 package br.com.jardessouza;
 
+import br.com.jardessouza.domain.Chamado;
+import br.com.jardessouza.domain.Cliente;
+import br.com.jardessouza.domain.Tecnico;
+import br.com.jardessouza.domain.enums.Perfil;
+import br.com.jardessouza.domain.enums.Prioridade;
+import br.com.jardessouza.domain.enums.Status;
+import br.com.jardessouza.repository.ChamadoRepository;
+import br.com.jardessouza.repository.ClienteRepository;
+import br.com.jardessouza.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelpdeskApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
     }
-
 }
